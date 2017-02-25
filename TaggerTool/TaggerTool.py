db7a5d63aa01b8e27480e99a435fc24bf3126006
Dmitry Misharov <quarckster@gmail.com>
2017-02-25 07:49:32 +0100

    Implemented filtering

--- TaggerTool/TaggerTool.py
+++ TaggerTool/TaggerTool.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 import sys
 import pandas
-from PyQt5 import QtCore, QtWidgets
+from PyQt5 import QtCore, QtWidgets, QtGui
 from matplotlib.backends.backend_qt5agg import (
     FigureCanvasQTAgg as FigureCanvas,
     NavigationToolbar2QT as NavigationToolbar
@@ -13,23 +13,40 @@ handata = pandas.read_csv("hanData_1464739200_1464825600.csv")
 frames = pandas.read_csv("frames_1464739200_1464825600.csv")
 x_handata = handata.Timestamp
 y_handata = handata.Value
-x_frames = frames.time
-y_frames_min = frames.plat[:-1]
-y_frames_max = frames.plat[1:]
-transisiton_colors = []
-for transition in frames.transition[1:]:
-    if transition > 0:
-        transisiton_colors.append("g")
-    elif transition < 0:
-        transisiton_colors.append("r")
-    else:
-        transisiton_colors.append("b")
+
+
+def get_frames(min_amp=None, max_amp=None):
+    if min_amp and max_amp:
+        filtered_frames = frames[
+            (frames.plat <= int(max_amp)) & (frames.plat >= int(min_amp))
+        ]
+    elif min_amp and not max_amp:
+        filtered_frames = frames[frames.plat >= int(min_amp)]
+    elif not min_amp and max_amp:
+        filtered_frames = frames[frames.plat <= int(max_amp)]
+    elif not min_amp and not max_amp:
+        filtered_frames = frames
+    x_frames = filtered_frames.time
+    y_frames_max = filtered_frames.plat
+    y_min_indexes = filtered_frames.index - 1
+    y_frames_min = frames.iloc[y_min_indexes].plat
+    transition_colors = []
+    for transition in filtered_frames.transition:
+        if transition > 0:
+            transition_colors.append("g")
+        elif transition < 0:
+            transition_colors.append("r")
+        else:
+            transition_colors.append("b")
+    return x_frames, y_frames_min, y_frames_max, transition_colors
 
 
 class Plot(FigureCanvas):
     def __init__(self, parent=None, width=5, height=10, dpi=100):
+        self.frames = None
+        self.axes = None
         self.fig = Figure(figsize=(width, height), dpi=dpi)
-        FigureCanvas.__init__(self, self.fig)
+        super().__init__(self.fig)
         self.mpl_connect("resize_event", self._tight_layout)
         self.build_plot()
         self.setParent(parent)
@@ -38,13 +55,20 @@ class Plot(FigureCanvas):
         self.fig.tight_layout()
 
     def build_plot(self):
-        axes = self.fig.add_subplot(111)
-        axes.plot(x_handata, y_handata, "b")
-        axes.vlines(x_frames, y_frames_min, y_frames_max, colors=transisiton_colors)
-        axes.set_xlabel("timestamps")
-        axes.set_ylabel("amplitude")
-        axes.grid(color="grey", linestyle="dotted", linewidth=0.25)
+        x_frames, y_frames_min, y_frames_max, transisiton_colors = get_frames()
+        self.axes = self.fig.add_subplot(111)
+        self.axes.plot(x_handata, y_handata, "b")
+        self.frames = self.axes.vlines(x_frames, y_frames_min, y_frames_max, colors=transisiton_colors)
+        self.axes.set_xlabel("timestamps")
+        self.axes.set_ylabel("amplitude")
+        self.axes.grid(color="grey", linestyle="dotted", linewidth=0.25)
         self._tight_layout()
+
+    def update_plot(self, min_amp=None, max_amp=None):
+        x_frames, y_frames_min, y_frames_max, transisiton_colors = get_frames(min_amp, max_amp)
+        self.frames.remove()
+        self.draw_idle()
+        self.frames = self.axes.vlines(x_frames, y_frames_min, y_frames_max, colors=transisiton_colors)
 
 
 class Ui_MainWindow(object):
@@ -92,20 +116,32 @@ class Ui_MainWindow(object):
         self.verticalLayout.setContentsMargins(11, 11, 11, 11)
         self.verticalLayout.setSpacing(6)
         self.verticalLayout.setObjectName("verticalLayout")
+        self.label = QtWidgets.QLabel(self.centralWidget)
+        self.label.setAlignment(QtCore.Qt.AlignCenter)
+        self.label.setObjectName("label")
+        self.verticalLayout.addWidget(self.label)
         self.horizontalLayout_2 = QtWidgets.QHBoxLayout()
         self.horizontalLayout_2.setContentsMargins(11, 11, 11, 11)
         self.horizontalLayout_2.setSpacing(6)
         self.horizontalLayout_2.setObjectName("horizontalLayout_2")
         self.min_amp = QtWidgets.QLabel(self.centralWidget)
+        sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Preferred, QtWidgets.QSizePolicy.Preferred)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(self.min_amp.sizePolicy().hasHeightForWidth())
+        self.min_amp.setSizePolicy(sizePolicy)
         self.min_amp.setObjectName("min_amp")
         self.horizontalLayout_2.addWidget(self.min_amp)
-        self.min_amp_val = QtWidgets.QLabel(self.centralWidget)
-        self.min_amp_val.setAlignment(QtCore.Qt.AlignRight | QtCore.Qt.AlignTrailing | QtCore.Qt.AlignVCenter)
-        self.min_amp_val.setObjectName("min_amp_val")
-        self.horizontalLayout_2.addWidget(self.min_amp_val)
+        self.min_amp_input = QtWidgets.QLineEdit(self.centralWidget)
+        self.min_amp_input.setValidator(QtGui.QIntValidator())
+        sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Minimum, QtWidgets.QSizePolicy.Fixed)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(self.min_amp_input.sizePolicy().hasHeightForWidth())
+        self.min_amp_input.setSizePolicy(sizePolicy)
+        self.min_amp_input.setObjectName("min_amp_input")
+        self.horizontalLayout_2.addWidget(self.min_amp_input)
         self.verticalLayout.addLayout(self.horizontalLayout_2)
-        spacerItem2 = QtWidgets.QSpacerItem(20, 40, QtWidgets.QSizePolicy.Minimum, QtWidgets.QSizePolicy.Expanding)
-        self.verticalLayout.addItem(spacerItem2)
         self.horizontalLayout_3 = QtWidgets.QHBoxLayout()
         self.horizontalLayout_3.setContentsMargins(11, 11, 11, 11)
         self.horizontalLayout_3.setSpacing(6)
@@ -113,47 +149,36 @@ class Ui_MainWindow(object):
         self.max_amp = QtWidgets.QLabel(self.centralWidget)
         self.max_amp.setObjectName("max_amp")
         self.horizontalLayout_3.addWidget(self.max_amp)
-        self.max_amp_val = QtWidgets.QLabel(self.centralWidget)
-        self.max_amp_val.setAlignment(QtCore.Qt.AlignRight | QtCore.Qt.AlignTrailing | QtCore.Qt.AlignVCenter)
-        self.max_amp_val.setObjectName("max_amp_val")
-        self.horizontalLayout_3.addWidget(self.max_amp_val)
+        self.max_amp_input = QtWidgets.QLineEdit(self.centralWidget)
+        self.max_amp_input.setValidator(QtGui.QIntValidator())
+        sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Minimum, QtWidgets.QSizePolicy.Fixed)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(self.max_amp_input.sizePolicy().hasHeightForWidth())
+        self.max_amp_input.setSizePolicy(sizePolicy)
+        self.max_amp_input.setObjectName("max_amp_input")
+        self.horizontalLayout_3.addWidget(self.max_amp_input)
         self.verticalLayout.addLayout(self.horizontalLayout_3)
-        spacerItem3 = QtWidgets.QSpacerItem(20, 40, QtWidgets.QSizePolicy.Minimum, QtWidgets.QSizePolicy.Expanding)
-        self.verticalLayout.addItem(spacerItem3)
-        self.horizontalLayout_4 = QtWidgets.QHBoxLayout()
-        self.horizontalLayout_4.setContentsMargins(11, 11, 11, 11)
-        self.horizontalLayout_4.setSpacing(6)
-        self.horizontalLayout_4.setObjectName("horizontalLayout_4")
-        self.min_dur = QtWidgets.QLabel(self.centralWidget)
-        self.min_dur.setObjectName("min_dur")
-        self.horizontalLayout_4.addWidget(self.min_dur)
-        self.max_dur_val_2 = QtWidgets.QLabel(self.centralWidget)
-        self.max_dur_val_2.setAlignment(QtCore.Qt.AlignRight | QtCore.Qt.AlignTrailing | QtCore.Qt.AlignVCenter)
-        self.max_dur_val_2.setObjectName("max_dur_val_2")
-        self.horizontalLayout_4.addWidget(self.max_dur_val_2)
-        self.verticalLayout.addLayout(self.horizontalLayout_4)
-        spacerItem4 = QtWidgets.QSpacerItem(20, 40, QtWidgets.QSizePolicy.Minimum, QtWidgets.QSizePolicy.Expanding)
-        self.verticalLayout.addItem(spacerItem4)
         self.horizontalLayout_5 = QtWidgets.QHBoxLayout()
         self.horizontalLayout_5.setContentsMargins(11, 11, 11, 11)
         self.horizontalLayout_5.setSpacing(6)
         self.horizontalLayout_5.setObjectName("horizontalLayout_5")
-        self.max_dur = QtWidgets.QLabel(self.centralWidget)
-        self.max_dur.setObjectName("max_dur")
-        self.horizontalLayout_5.addWidget(self.max_dur)
-        self.max_dur_val = QtWidgets.QLabel(self.centralWidget)
-        self.max_dur_val.setAlignment(QtCore.Qt.AlignRight | QtCore.Qt.AlignTrailing | QtCore.Qt.AlignVCenter)
-        self.max_dur_val.setObjectName("max_dur_val")
-        self.horizontalLayout_5.addWidget(self.max_dur_val)
+        self.submit_button = QtWidgets.QPushButton(self.centralWidget)
+        self.submit_button.clicked.connect(self.handleButton)
+        sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Preferred, QtWidgets.QSizePolicy.Fixed)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(self.submit_button.sizePolicy().hasHeightForWidth())
+        self.submit_button.setSizePolicy(sizePolicy)
+        self.submit_button.setBaseSize(QtCore.QSize(0, 0))
+        self.submit_button.setLayoutDirection(QtCore.Qt.LeftToRight)
+        self.submit_button.setObjectName("submit_button")
+        self.horizontalLayout_5.addWidget(self.submit_button)
         self.verticalLayout.addLayout(self.horizontalLayout_5)
+        spacerItem3 = QtWidgets.QSpacerItem(20, 40, QtWidgets.QSizePolicy.Minimum, QtWidgets.QSizePolicy.Expanding)
+        self.verticalLayout.addItem(spacerItem3)
         self.gridLayout.addLayout(self.verticalLayout, 0, 1, 1, 1)
         self.gridLayout_2.addLayout(self.gridLayout, 0, 0, 1, 1)
-        self.prev_day_button.raise_()
-        self.next_day_button.raise_()
-        self.save_button.raise_()
-        self.min_amp_val.raise_()
-        self.min_amp.raise_()
-        self.plot.raise_()
         MainWindow.setCentralWidget(self.centralWidget)
         self.statusBar = QtWidgets.QStatusBar(MainWindow)
         self.statusBar.setObjectName("statusBar")
@@ -172,10 +197,16 @@ class Ui_MainWindow(object):
         self.menuBar.addAction(self.menuFile.menuAction())
         self.menuBar.addAction(self.menuAbout.menuAction())
         self.plot_toolbar = NavigationToolbar(self.plot, MainWindow)
+        self.plot_toolbar.setObjectName("Matplolib toolbar")
         MainWindow.addToolBar(self.plot_toolbar)
 
         self.retranslateUi(MainWindow)
         QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def handleButton(self):
+        min_amp = self.min_amp_input.text()
+        max_amp = self.max_amp_input.text()
+        self.plot.update_plot(min_amp, max_amp)
 
     def retranslateUi(self, MainWindow):
         _translate = QtCore.QCoreApplication.translate
@@ -183,16 +214,13 @@ class Ui_MainWindow(object):
         self.prev_day_button.setText(_translate("MainWindow", "Previous day"))
         self.save_button.setText(_translate("MainWindow", "Save"))
         self.next_day_button.setText(_translate("MainWindow", "Next day"))
+        self.label.setText(_translate("MainWindow", "Filter amplitude"))
         self.min_amp.setText(_translate("MainWindow", "Min amp:"))
-        self.min_amp_val.setText(_translate("MainWindow", "TextLabel"))
         self.max_amp.setText(_translate("MainWindow", "Max amp:"))
-        self.max_amp_val.setText(_translate("MainWindow", "TextLabel"))
-        self.min_dur.setText(_translate("MainWindow", "Min duration:"))
-        self.max_dur_val_2.setText(_translate("MainWindow", "TextLabel"))
-        self.max_dur.setText(_translate("MainWindow", "Max duration:"))
-        self.max_dur_val.setText(_translate("MainWindow", "TextLabel"))
+        self.submit_button.setText(_translate("MainWindow", "Submit"))
         self.menuFile.setTitle(_translate("MainWindow", "&File"))
         self.menuAbout.setTitle(_translate("MainWindow", "Abo&ut"))
+        self.plot_toolbar.setWindowTitle(_translate("MainWindow", "Matplolib toolbar"))
         self.actionExit.setText(_translate("MainWindow", "&Exit"))
 
 
